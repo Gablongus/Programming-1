dce44d62f9cca17eb22c6a42fc46a93a045a3576
Gablongus <schraedl.g@stu.janesville.k12.wi.us>
2024-10-18 15:27:40 -0500

    10/18/24

---
+++ Prog 122b/Prog 122b/MainForm.py
@@ -0,0 +1,109 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._listBox1 = System.Windows.Forms.ListBox()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self._label1 = System.Windows.Forms.Label()
+        self.SuspendLayout()
+        # 
+        # listBox1
+        # 
+        self._listBox1.Font = System.Drawing.Font("MS Reference Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._listBox1.FormattingEnabled = True
+        self._listBox1.ItemHeight = 20
+        self._listBox1.Location = System.Drawing.Point(208, 61)
+        self._listBox1.Name = "listBox1"
+        self._listBox1.Size = System.Drawing.Size(196, 224)
+        self._listBox1.TabIndex = 0
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.YellowGreen
+        self._button1.Font = System.Drawing.Font("Myanmar Text", 14.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.ForeColor = System.Drawing.Color.White
+        self._button1.Location = System.Drawing.Point(12, 60)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(190, 71)
+        self._button1.TabIndex = 1
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.YellowGreen
+        self._button2.Font = System.Drawing.Font("Myanmar Text", 14.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.ForeColor = System.Drawing.Color.White
+        self._button2.Location = System.Drawing.Point(12, 137)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(190, 71)
+        self._button2.TabIndex = 2
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.YellowGreen
+        self._button3.Font = System.Drawing.Font("Myanmar Text", 14.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.ForeColor = System.Drawing.Color.White
+        self._button3.Location = System.Drawing.Point(12, 214)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(190, 71)
+        self._button3.TabIndex = 3
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.Color.LimeGreen
+        self._label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        self._label1.Font = System.Drawing.Font("Mongolian Baiti", 21.75, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        self._label1.Location = System.Drawing.Point(13, 13)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(391, 44)
+        self._label1.TabIndex = 4
+        self._label1.Text = "Hourly Pay"
+        self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.ForestGreen
+        self.ClientSize = System.Drawing.Size(416, 297)
+        self.Controls.Add(self._label1)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._listBox1)
+        self.Name = "MainForm"
+        self.Text = "Prog122b"
+        self.ResumeLayout(False)
+
+
+    
+    def Button1Click(self, sender, e):
+        Hours = 0
+        self._listBox1.Items.Add("Hours\tPay")
+        while Hours <= 40:
+            Pay = Hours * 4
+            Line = str(Hours) + "\t" + str(Pay)
+            self._listBox1.Items.Add(Line)
+            Hours = Hours + 1
+    
+    def Button2Click(self, sender, e):
+        self._listBox1.Items.Clear()
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
---
+++ Prog 122c/Prog 122c/MainForm.py
@@ -0,0 +1,99 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._listBox1 = System.Windows.Forms.ListBox()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # listBox1
+        # 
+        self._listBox1.Font = System.Drawing.Font("Microsoft YaHei UI", 11.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._listBox1.FormattingEnabled = True
+        self._listBox1.ItemHeight = 19
+        self._listBox1.Location = System.Drawing.Point(223, 12)
+        self._listBox1.Name = "listBox1"
+        self._listBox1.Size = System.Drawing.Size(244, 232)
+        self._listBox1.TabIndex = 0
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.DeepSkyBlue
+        self._button1.Font = System.Drawing.Font("Mongolian Baiti", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        self._button1.Location = System.Drawing.Point(12, 12)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(205, 74)
+        self._button1.TabIndex = 1
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.DeepSkyBlue
+        self._button2.Font = System.Drawing.Font("Mongolian Baiti", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        self._button2.Location = System.Drawing.Point(12, 92)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(205, 73)
+        self._button2.TabIndex = 2
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.DeepSkyBlue
+        self._button3.Font = System.Drawing.Font("Mongolian Baiti", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        self._button3.Location = System.Drawing.Point(12, 171)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(205, 73)
+        self._button3.TabIndex = 3
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.DodgerBlue
+        self.ClientSize = System.Drawing.Size(482, 252)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._listBox1)
+        self.Name = "MainForm"
+        self.Text = "Prog 122c"
+        self.Load += self.MainFormLoad
+        self.ResumeLayout(False)
+
+
+    def MainFormLoad(self, sender, e):
+        pass
+
+    def Button1Click(self, sender, e):
+        #Pattern is Number 1 increase + 2.Number 1 + 1 = Number 2, (Number 2 - 2) + 
+        #Number 2 = Number 3, Number 3 * X = Number 4. Increase X
+        Increment4 = 
+        Num1 = 2
+        Num2 = Num1 + 1
+        Num3 = Num2 + (Num2 - 2)
+        Num4 = Num3 * Increment 4 
+        Line = str(Num1) + "\t" + str(Num2) + "\t" + str(Num3) + "\t" + str(Num4)
+                
+
+    def Button2Click(self, sender, e):
+        pass
+
+    def Button3Click(self, sender, e):
+        pass
