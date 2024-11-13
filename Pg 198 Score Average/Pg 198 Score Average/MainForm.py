e99d44f6a57605ee34af1be456e8652c2fd04abe
Gablongus <schraedl.g@stu.janesville.k12.wi.us>
2024-11-13 15:26:05 -0600

    11/13/24

--- Pg 198 Score Average/Pg 198 Score Average/MainForm.py
+++ Pg 198 Score Average/Pg 198 Score Average/MainForm.py
@@ -18,11 +18,16 @@ class MainForm(Form):
         self._textBox3 = System.Windows.Forms.TextBox()
         self._label5 = System.Windows.Forms.Label()
         self._label6 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self._label7 = System.Windows.Forms.Label()
         self.SuspendLayout()
         # 
         # label1
         # 
         self._label1.BackColor = System.Drawing.Color.CornflowerBlue
+        self._label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         self._label1.Font = System.Drawing.Font("Ebrima", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
         self._label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
         self._label1.Location = System.Drawing.Point(12, 9)
@@ -109,10 +114,60 @@ class MainForm(Form):
         self._label6.Size = System.Drawing.Size(106, 29)
         self._label6.TabIndex = 8
         # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.LightSteelBlue
+        self._button1.Font = System.Drawing.Font("Ebrima", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        self._button1.Location = System.Drawing.Point(11, 357)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(193, 67)
+        self._button1.TabIndex = 9
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.LightSteelBlue
+        self._button2.Font = System.Drawing.Font("Ebrima", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        self._button2.Location = System.Drawing.Point(210, 357)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(200, 67)
+        self._button2.TabIndex = 10
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.LightSteelBlue
+        self._button3.Font = System.Drawing.Font("Ebrima", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        self._button3.Location = System.Drawing.Point(102, 430)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(200, 67)
+        self._button3.TabIndex = 11
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        # 
+        # label7
+        # 
+        self._label7.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        self._label7.Location = System.Drawing.Point(127, 331)
+        self._label7.Name = "label7"
+        self._label7.Size = System.Drawing.Size(141, 23)
+        self._label7.TabIndex = 12
+        self._label7.Text = "Congratulations! Great Job!"
+        # 
         # MainForm
         # 
         self.BackColor = System.Drawing.Color.RoyalBlue
-        self.ClientSize = System.Drawing.Size(429, 564)
+        self.ClientSize = System.Drawing.Size(429, 499)
+        self.Controls.Add(self._label7)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
         self.Controls.Add(self._label6)
         self.Controls.Add(self._label5)
         self.Controls.Add(self._textBox3)
@@ -127,3 +182,7 @@ class MainForm(Form):
         self.ResumeLayout(False)
         self.PerformLayout()
 
+
+    def Button1Click(self, sender, e):
+        box1 = str(self._textBox1.Text)
+        box2 = str
