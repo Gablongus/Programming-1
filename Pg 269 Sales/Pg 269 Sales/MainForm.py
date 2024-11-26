924ba7f59073ada76f586a06d1a3767eb9424fc7
Gablongus <schraedl.g@stu.janesville.k12.wi.us>
2024-11-26 15:23:04 -0600

    11/26/24

--- Pg 269 Sales/Pg 269 Sales/MainForm.py
+++ Pg 269 Sales/Pg 269 Sales/MainForm.py
@@ -10,7 +10,6 @@ class MainForm(Form):
     
     def InitializeComponent(self):
         self._label1 = System.Windows.Forms.Label()
-        self._listBox1 = System.Windows.Forms.ListBox()
         self._label2 = System.Windows.Forms.Label()
         self._label3 = System.Windows.Forms.Label()
         self._label4 = System.Windows.Forms.Label()
@@ -20,6 +19,7 @@ class MainForm(Form):
         self._button1 = System.Windows.Forms.Button()
         self._button2 = System.Windows.Forms.Button()
         self._button3 = System.Windows.Forms.Button()
+        self._label5 = System.Windows.Forms.Label()
         self.SuspendLayout()
         # 
         # label1
@@ -32,14 +32,6 @@ class MainForm(Form):
         self._label1.Size = System.Drawing.Size(211, 133)
         self._label1.TabIndex = 0
         self._label1.Text = "Quantity Sold:"
-        # 
-        # listBox1
-        # 
-        self._listBox1.FormattingEnabled = True
-        self._listBox1.Location = System.Drawing.Point(254, 15)
-        self._listBox1.Name = "listBox1"
-        self._listBox1.Size = System.Drawing.Size(163, 251)
-        self._listBox1.TabIndex = 1
         # 
         # label2
         # 
@@ -114,6 +106,7 @@ class MainForm(Form):
         self._button2.TabIndex = 9
         self._button2.Text = "Clear"
         self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
         # 
         # button3
         # 
@@ -125,11 +118,23 @@ class MainForm(Form):
         self._button3.TabIndex = 10
         self._button3.Text = "Exit"
         self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
+        # label5
+        # 
+        self._label5.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        self._label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        self._label5.Font = System.Drawing.Font("Microsoft Yi Baiti", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label5.Location = System.Drawing.Point(240, 18)
+        self._label5.Name = "label5"
+        self._label5.Size = System.Drawing.Size(177, 239)
+        self._label5.TabIndex = 11
         # 
         # MainForm
         # 
         self.BackColor = System.Drawing.SystemColors.ActiveCaption
         self.ClientSize = System.Drawing.Size(429, 274)
+        self.Controls.Add(self._label5)
         self.Controls.Add(self._button3)
         self.Controls.Add(self._button2)
         self.Controls.Add(self._button1)
@@ -139,11 +144,9 @@ class MainForm(Form):
         self.Controls.Add(self._label4)
         self.Controls.Add(self._label3)
         self.Controls.Add(self._label2)
-        self.Controls.Add(self._listBox1)
         self.Controls.Add(self._label1)
         self.Name = "MainForm"
         self.Text = "Pg 269 Sales"
-        self.Load += self.MainFormLoad
         self.ResumeLayout(False)
         self.PerformLayout()
 
@@ -152,6 +155,66 @@ class MainForm(Form):
 
     def Button1Click(self, sender, e):
         #Todo: All of this
-        A
-        B
-        C
+        Aamount = int(self._textBox1.Text)
+        Bamount = int(self._textBox2.Text)
+        Camount = int(self._textBox3.Text)
+        
+        Adiscount = 0
+        Bdiscount = 0
+        Cdiscount = 0
+        
+        Aprice = 0
+        Bprice = 0
+        Cprice = 0
+        
+        if Aamount >= 10 and Aamount <= 19:
+            Adiscount = 0.20
+        elif Aamount >= 20 and Aamount <= 49:
+            Adiscount = 0.30
+        elif Aamount >= 50 and Aamount <= 99:
+            Adiscount = 0.40
+        elif Aamount >= 100:
+            Adiscount = 0.50
+            
+        if Bamount >= 10 and Bamount <= 19:
+            Bdiscount = 0.20
+        elif Bamount >= 20 and Bamount <= 49:
+            Bdiscount = 0.30
+        elif Bamount >= 50 and Bamount <= 99:
+            Bdiscount = 0.40
+        elif Bamount >= 100:
+            Bdiscount = 0.50
+        
+        if Camount >= 10 and Camount <= 19:
+            Cdiscount = 0.20
+        elif Camount >= 20 and Camount <= 49:
+            Cdiscount = 0.30
+        elif Camount >= 50 and Camount <= 99:
+            Cdiscount = 0.40
+        elif Camount >= 100:
+            Cdiscount = 0.50
+            
+        Aprice = (Aamount * 99) - ((Aamount * 99)*Adiscount)
+        Bprice = (Bamount * 199) - ((Bamount * 199)*Bdiscount)
+        Cprice = (Camount * 299) - ((Camount * 299)*Cdiscount)
+            
+        
+        
+        
+        self._label5.Text += ("Package A: $" + str(Aprice))
+        self._label5.Text += ("\nPackage B: $" + str(Bprice))
+        self._label5.Text += ("\nPackage C: $" + str(Cprice))
+        self._label5.Text += ("\nGrand Total: $" + str(Aprice + Bprice + Cprice))
+        
+        
+        
+        
+
+    def Button2Click(self, sender, e):
+        self._label5.Text = ""
+        self._textBox1.Text = ""
+        self._textBox2.Text = ""
+        self._textBox3.Text = ""
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
