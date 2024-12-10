380ff92ce0dfc0483ec7741cfc8fa706a13c9ebb
Gablongus <schraedl.g@stu.janesville.k12.wi.us>
2024-12-10 15:24:45 -0600

    12/10/24

--- Pg 162 Room Charge/Pg 162 Room Charge/MainForm.py
+++ Pg 162 Room Charge/Pg 162 Room Charge/MainForm.py
@@ -53,7 +53,7 @@ class MainForm(Form):
         # 
         self._label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         self._label2.Font = System.Drawing.Font("Microsoft YaHei Light", 9, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
-        self._label2.Location = System.Drawing.Point(28, 72)
+        self._label2.Location = System.Drawing.Point(28, 95)
         self._label2.Name = "label2"
         self._label2.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label2.Size = System.Drawing.Size(164, 104)
@@ -64,7 +64,7 @@ class MainForm(Form):
         # 
         self._label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         self._label3.Font = System.Drawing.Font("Microsoft YaHei Light", 9, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
-        self._label3.Location = System.Drawing.Point(231, 72)
+        self._label3.Location = System.Drawing.Point(231, 95)
         self._label3.Name = "label3"
         self._label3.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label3.Size = System.Drawing.Size(168, 104)
@@ -74,7 +74,7 @@ class MainForm(Form):
         # label5
         # 
         self._label5.Font = System.Drawing.Font("Mongolian Baiti", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label5.Location = System.Drawing.Point(31, 126)
+        self._label5.Location = System.Drawing.Point(31, 149)
         self._label5.Name = "label5"
         self._label5.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label5.Size = System.Drawing.Size(97, 16)
@@ -85,7 +85,7 @@ class MainForm(Form):
         # label6
         # 
         self._label6.Font = System.Drawing.Font("Mongolian Baiti", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label6.Location = System.Drawing.Point(248, 99)
+        self._label6.Location = System.Drawing.Point(248, 122)
         self._label6.Name = "label6"
         self._label6.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label6.Size = System.Drawing.Size(89, 16)
@@ -96,7 +96,7 @@ class MainForm(Form):
         # label7
         # 
         self._label7.Font = System.Drawing.Font("Mongolian Baiti", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label7.Location = System.Drawing.Point(261, 126)
+        self._label7.Location = System.Drawing.Point(261, 149)
         self._label7.Name = "label7"
         self._label7.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label7.Size = System.Drawing.Size(76, 16)
@@ -107,7 +107,7 @@ class MainForm(Form):
         # label8
         # 
         self._label8.Font = System.Drawing.Font("Mongolian Baiti", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label8.Location = System.Drawing.Point(279, 151)
+        self._label8.Location = System.Drawing.Point(279, 174)
         self._label8.Name = "label8"
         self._label8.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label8.Size = System.Drawing.Size(58, 16)
@@ -119,7 +119,7 @@ class MainForm(Form):
         # 
         self._label9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         self._label9.Font = System.Drawing.Font("Microsoft YaHei UI", 9.75, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
-        self._label9.Location = System.Drawing.Point(28, 192)
+        self._label9.Location = System.Drawing.Point(28, 215)
         self._label9.Name = "label9"
         self._label9.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label9.Size = System.Drawing.Size(371, 193)
@@ -129,7 +129,7 @@ class MainForm(Form):
         # label10
         # 
         self._label10.Font = System.Drawing.Font("Mongolian Baiti", 11.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label10.Location = System.Drawing.Point(107, 218)
+        self._label10.Location = System.Drawing.Point(107, 241)
         self._label10.Name = "label10"
         self._label10.Size = System.Drawing.Size(135, 23)
         self._label10.TabIndex = 9
@@ -139,7 +139,7 @@ class MainForm(Form):
         # label11
         # 
         self._label11.Font = System.Drawing.Font("Mongolian Baiti", 11.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label11.Location = System.Drawing.Point(107, 250)
+        self._label11.Location = System.Drawing.Point(107, 273)
         self._label11.Name = "label11"
         self._label11.Size = System.Drawing.Size(135, 23)
         self._label11.TabIndex = 10
@@ -149,7 +149,7 @@ class MainForm(Form):
         # label12
         # 
         self._label12.Font = System.Drawing.Font("Mongolian Baiti", 11.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label12.Location = System.Drawing.Point(108, 282)
+        self._label12.Location = System.Drawing.Point(108, 305)
         self._label12.Name = "label12"
         self._label12.Size = System.Drawing.Size(135, 23)
         self._label12.TabIndex = 11
@@ -159,7 +159,7 @@ class MainForm(Form):
         # label13
         # 
         self._label13.Font = System.Drawing.Font("Mongolian Baiti", 11.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label13.Location = System.Drawing.Point(107, 315)
+        self._label13.Location = System.Drawing.Point(107, 338)
         self._label13.Name = "label13"
         self._label13.Size = System.Drawing.Size(135, 23)
         self._label13.TabIndex = 12
@@ -169,7 +169,7 @@ class MainForm(Form):
         # label14
         # 
         self._label14.Font = System.Drawing.Font("Mongolian Baiti", 11.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label14.Location = System.Drawing.Point(107, 348)
+        self._label14.Location = System.Drawing.Point(107, 371)
         self._label14.Name = "label14"
         self._label14.Size = System.Drawing.Size(135, 23)
         self._label14.TabIndex = 13
@@ -181,7 +181,7 @@ class MainForm(Form):
         self._label15.BackColor = System.Drawing.Color.White
         self._label15.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         self._label15.Font = System.Drawing.Font("Microsoft YaHei", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label15.Location = System.Drawing.Point(248, 218)
+        self._label15.Location = System.Drawing.Point(248, 241)
         self._label15.Name = "label15"
         self._label15.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label15.Size = System.Drawing.Size(100, 23)
@@ -193,7 +193,7 @@ class MainForm(Form):
         self._label16.BackColor = System.Drawing.Color.White
         self._label16.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         self._label16.Font = System.Drawing.Font("Microsoft YaHei", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label16.Location = System.Drawing.Point(248, 250)
+        self._label16.Location = System.Drawing.Point(248, 273)
         self._label16.Name = "label16"
         self._label16.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label16.Size = System.Drawing.Size(100, 23)
@@ -205,7 +205,7 @@ class MainForm(Form):
         self._label17.BackColor = System.Drawing.Color.White
         self._label17.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         self._label17.Font = System.Drawing.Font("Microsoft YaHei", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label17.Location = System.Drawing.Point(249, 282)
+        self._label17.Location = System.Drawing.Point(249, 305)
         self._label17.Name = "label17"
         self._label17.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label17.Size = System.Drawing.Size(100, 23)
@@ -217,7 +217,7 @@ class MainForm(Form):
         self._label18.BackColor = System.Drawing.Color.White
         self._label18.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         self._label18.Font = System.Drawing.Font("Microsoft YaHei", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label18.Location = System.Drawing.Point(248, 315)
+        self._label18.Location = System.Drawing.Point(248, 338)
         self._label18.Name = "label18"
         self._label18.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label18.Size = System.Drawing.Size(100, 23)
@@ -229,7 +229,7 @@ class MainForm(Form):
         self._label19.BackColor = System.Drawing.Color.White
         self._label19.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         self._label19.Font = System.Drawing.Font("Microsoft YaHei", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label19.Location = System.Drawing.Point(248, 348)
+        self._label19.Location = System.Drawing.Point(248, 371)
         self._label19.Name = "label19"
         self._label19.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label19.Size = System.Drawing.Size(100, 23)
@@ -239,7 +239,7 @@ class MainForm(Form):
         # label4
         # 
         self._label4.Font = System.Drawing.Font("Mongolian Baiti", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label4.Location = System.Drawing.Point(39, 99)
+        self._label4.Location = System.Drawing.Point(39, 122)
         self._label4.Name = "label4"
         self._label4.RightToLeft = System.Windows.Forms.RightToLeft.No
         self._label4.Size = System.Drawing.Size(58, 16)
@@ -251,17 +251,18 @@ class MainForm(Form):
         # 
         self._button1.BackColor = System.Drawing.Color.Azure
         self._button1.Font = System.Drawing.Font("Baskerville Old Face", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._button1.Location = System.Drawing.Point(28, 399)
+        self._button1.Location = System.Drawing.Point(28, 422)
         self._button1.Name = "button1"
         self._button1.Size = System.Drawing.Size(112, 39)
         self._button1.TabIndex = 20
         self._button1.Text = "Calculate Charges"
         self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
         # 
         # textBox1
         # 
         self._textBox1.Font = System.Drawing.Font("Microsoft YaHei", 8.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._textBox1.Location = System.Drawing.Point(103, 97)
+        self._textBox1.Location = System.Drawing.Point(103, 120)
         self._textBox1.Name = "textBox1"
         self._textBox1.Size = System.Drawing.Size(78, 22)
         self._textBox1.TabIndex = 23
@@ -271,28 +272,30 @@ class MainForm(Form):
         # 
         self._button2.BackColor = System.Drawing.Color.Azure
         self._button2.Font = System.Drawing.Font("Baskerville Old Face", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._button2.Location = System.Drawing.Point(157, 399)
+        self._button2.Location = System.Drawing.Point(157, 422)
         self._button2.Name = "button2"
         self._button2.Size = System.Drawing.Size(112, 39)
         self._button2.TabIndex = 24
         self._button2.Text = "Clear"
         self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
         # 
         # button3
         # 
         self._button3.BackColor = System.Drawing.Color.Azure
         self._button3.Font = System.Drawing.Font("Baskerville Old Face", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._button3.Location = System.Drawing.Point(287, 399)
+        self._button3.Location = System.Drawing.Point(287, 422)
         self._button3.Name = "button3"
         self._button3.Size = System.Drawing.Size(112, 39)
         self._button3.TabIndex = 25
         self._button3.Text = "Exit"
         self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
         # 
         # textBox2
         # 
         self._textBox2.Font = System.Drawing.Font("Microsoft YaHei", 8.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._textBox2.Location = System.Drawing.Point(134, 126)
+        self._textBox2.Location = System.Drawing.Point(134, 149)
         self._textBox2.Name = "textBox2"
         self._textBox2.Size = System.Drawing.Size(47, 22)
         self._textBox2.TabIndex = 26
@@ -301,7 +304,7 @@ class MainForm(Form):
         # textBox3
         # 
         self._textBox3.Font = System.Drawing.Font("Microsoft YaHei", 8.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._textBox3.Location = System.Drawing.Point(339, 96)
+        self._textBox3.Location = System.Drawing.Point(339, 119)
         self._textBox3.Name = "textBox3"
         self._textBox3.Size = System.Drawing.Size(51, 22)
         self._textBox3.TabIndex = 27
@@ -310,7 +313,7 @@ class MainForm(Form):
         # textBox4
         # 
         self._textBox4.Font = System.Drawing.Font("Microsoft YaHei", 8.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._textBox4.Location = System.Drawing.Point(339, 123)
+        self._textBox4.Location = System.Drawing.Point(339, 146)
         self._textBox4.Name = "textBox4"
         self._textBox4.Size = System.Drawing.Size(51, 22)
         self._textBox4.TabIndex = 28
@@ -319,7 +322,7 @@ class MainForm(Form):
         # textBox5
         # 
         self._textBox5.Font = System.Drawing.Font("Microsoft YaHei", 8.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._textBox5.Location = System.Drawing.Point(339, 149)
+        self._textBox5.Location = System.Drawing.Point(339, 172)
         self._textBox5.Name = "textBox5"
         self._textBox5.Size = System.Drawing.Size(51, 22)
         self._textBox5.TabIndex = 29
@@ -328,7 +331,7 @@ class MainForm(Form):
         # MainForm
         # 
         self.BackColor = System.Drawing.Color.LightCyan
-        self.ClientSize = System.Drawing.Size(427, 450)
+        self.ClientSize = System.Drawing.Size(427, 469)
         self.Controls.Add(self._textBox5)
         self.Controls.Add(self._textBox4)
         self.Controls.Add(self._textBox3)
@@ -365,4 +368,26 @@ class MainForm(Form):
 
 
     def MainFormLoad(self, sender, e):
+        pass
+
+    def Button1Click(self, sender, e):
+        decRoomCharges = 0.0
+        decAddCharges = 0.0
+        decSubTotal = 0.0
+        decTax = 0.0
+        decTotal = 0.0
+        decTAX_RATE = 0.08
+        
+        try:
+            decAddCharges = float(self._textBox3.Text) + \
+                float(self._textBox4.Text) + \
+                float(self._textBox5.Text0
+            self._lblAddCharges.Text = str(round(decAddCharges, 2))
+        except:
+            MessageBox.Show("Room service, Telephone, and Misc. must be numbers", "Error")
+
+    def Button2Click(self, sender, e):
+        pass
+
+    def Button3Click(self, sender, e):
         pass
